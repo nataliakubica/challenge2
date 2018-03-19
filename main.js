7376255d89ae7281452d2a6082f538cb6f1bea78
Natalia <nataliakubica@icloud.com>
2018-03-19 14:06:11 +0100

    Editted operators

--- main.js
+++ main.js
@@ -6,31 +6,31 @@ function updateClock() {
 
 	document.getElementById('tijd').innerHTML = currentTime.toLocaleTimeString();
 	
-	if (currentTime.getHours() == 1 && currentTime.getHours() > 8) {
+	if (currentTime.getHours() >= 1 && currentTime.getHours() <= 8) {
 		document.getElementById("tekst").innerHTML = "Het is tijd om te slapen"; 
         document.body.style.background = "#80A3B2";
         document.body.style.fontFamily = "Silom";
 	}
 
-	if (currentTime.getHours() == 8 && currentTime.getHours() < 11) {
+	if (currentTime.getHours() >= 8 && currentTime.getHours() <= 11) {
 		 document.getElementById("tekst").innerHTML = "Het is tijd om te ontbijten";
 		 document.body.style.background = "#B77E8D";
         document.body.style.fontFamily = "Silom";
 	}
 
-	if (currentTime.getHours() == 11 && currentTime.getHours() < 16) {
+	if (currentTime.getHours() >= 11 && currentTime.getHours() <= 16) {
 		 document.getElementById("tekst").innerHTML = "Het is tijd om te lunchen";
 		 document.body.style.background = "#AE897E";
         document.body.style.fontFamily = "Silom";
 	}
     
-    if (currentTime.getHours() == 16 && currentTime.getHours() < 19) {
+    if (currentTime.getHours() >= 16 && currentTime.getHours() <= 19) {
 		 document.getElementById("tekst").innerHTML = "Het is tijd om avond te eten";
 		 document.body.style.background = "#93AF80";
         document.body.style.fontFamily = "Silom";
 	}
     
-    if (currentTime.getHours() == 19 && currentTime.getHours() < 24) {
+    if (currentTime.getHours() >= 19 && currentTime.getHours() <= 24) {
 		 document.getElementById("tekst").innerHTML = "Het is tijd om te slapen";
 		 document.body.style.background = "#80A3B2";
         document.body.style.fontFamily = "Silom";
